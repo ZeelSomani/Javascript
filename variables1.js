f188884cb6fbe502df0480ac994736391af1a82d
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2025-06-25 11:52:35 +0530

    Create variables1.js

---
+++ variables1.js
@@ -0,0 +1,17 @@
+<html>
+    <body>
+        <script>
+            var firstname;
+            firstname="Welcome";
+            document.write(firstname);
+            document.write("<br/>");
+            firstname="XYZ";
+            document.write(firstname);
+        </script>
+        <p>
+            The script above declares a variable ,
+            assigns a value to it, displays the value, change the value,
+            and displays the value again.
+        </p>
+    </body>
+</html>
